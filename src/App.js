2848406922691ad62af84a93950f6f247050a9de
SteveLigin <liginkiller@.net>
2023-08-27 18:47:59 -0700

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
+import { Routes,Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Placetovisit from './componentvisit/Placetovisit';
+
+
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <BrowserRouter>
+     <div>
+     
+     <Navbar/>
+     
+     <Routes>
+     <Route path="/" element={<Home />}>
+        
+        </Route>
+     <Route path="/placetovisit" element={<Placetovisit />}>
+        
+        </Route>              
+        </Routes>
+       
+     </div>
+       
+    </BrowserRouter>
+
+
+   
+     
+
+    
+    
+
+  )
 }
+
 
 export default App;
---
+++ src/componentvisit/Placetovisit.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Start from '../componentvisit/Start';
+import Chennai from '../componentvisit/Chennai';
+import Rameshwaram from '../componentvisit/Rameshwaram';
+import Kodaikanal from '../componentvisit/Kodaikanal';
+import Ooty from '../componentvisit/Ooty';
+import Kanyakumari from '../componentvisit/Kanyakumari';
+import Kumbakonam from '../componentvisit/Kumbakonam';
+import Yercaud from '../componentvisit/Yercaud';
+import Theni from '../componentvisit/Theni';
+import Madurai from '../componentvisit/Madurai';
+import Hogenakal from '../componentvisit/Hogenakal';
+
+export default function Placetovisit() {
+  return (
+    <div>
+       <Start/>
+       <Chennai/>
+       <Rameshwaram/>
+       <Kodaikanal/>
+       <Ooty/>
+       <Kanyakumari/>
+       <Kumbakonam/>
+       <Madurai/>
+       <Yercaud/>
+       <Theni/>
+       <Hogenakal/>
+    </div>
+  )
+}
